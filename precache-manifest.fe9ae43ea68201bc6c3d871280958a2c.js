6962e62f6828db9e64fe64653c257d6430c04bf3
KAPIL <kapil992000@gmail.com>
2020-09-23 14:44:54 +0530

    Updates

---
+++ precache-manifest.fe9ae43ea68201bc6c3d871280958a2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1aa9dfd35f9b80c175554eee767fddc",
+    "url": "/covid19-data-app/index.html"
+  },
+  {
+    "revision": "7bbc6ba92ee258ff5992",
+    "url": "/covid19-data-app/static/css/main.951633a9.chunk.css"
+  },
+  {
+    "revision": "93066652cbcc4eb392a4",
+    "url": "/covid19-data-app/static/js/2.5ac3ca1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-data-app/static/js/2.5ac3ca1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbc6ba92ee258ff5992",
+    "url": "/covid19-data-app/static/js/main.d3032f8f.chunk.js"
+  },
+  {
+    "revision": "1282cddac6f26cce2907",
+    "url": "/covid19-data-app/static/js/runtime-main.6a9eb590.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19-data-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
